0ddf38c34c47d91ca5524b74dd49e95ca2f1be35
Niek <niek.devries@hotmail.com>
2021-12-02 17:27:37 +0100

    Day 2

---
+++ src/main/java/Day2/Dive.java
@@ -0,0 +1,43 @@
+package Day2;
+
+import Util.InputHandler;
+
+import java.util.stream.Stream;
+
+public class Dive {
+    private static int dive(Stream<String> inputs) {
+        int horizontal_pos = 0, depth = 0;
+        for (String[] input : inputs.map(s -> s.split(" ")).toList()) {
+            switch(input[0]) {
+                case "forward" -> horizontal_pos += Integer.parseInt(input[1]);
+                case "down" -> depth += Integer.parseInt(input[1]);
+                case "up" -> depth -= Integer.parseInt(input[1]);
+                default -> throw new IllegalStateException("Unexpected value: " + input[0]);
+            }
+        }
+        System.out.println("horizontal position: " + horizontal_pos + ", depth: " + depth);
+        return horizontal_pos * depth;
+    }
+
+    private static int diveWithAim(Stream<String> inputs) {
+        int horizontal_pos = 0, depth = 0, aim = 0;
+        for (String[] input : inputs.map(s -> s.split(" ")).toList()) {
+            switch(input[0]) {
+                case "forward" -> {
+                    horizontal_pos += Integer.parseInt(input[1]);
+                    depth +=  aim * Integer.parseInt(input[1]);
+                }
+                case "down" -> aim += Integer.parseInt(input[1]);
+                case "up" -> aim -= Integer.parseInt(input[1]);
+                default -> throw new IllegalStateException("Unexpected value: " + input[0]);
+            }
+        }
+        System.out.println("horizontal position: " + horizontal_pos + ", depth: " + depth);
+        return horizontal_pos * depth;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("horizontal position * depth = " + dive(InputHandler.getStringStream("Day2/Dive.txt")));
+        System.out.println("horizontal position * depth = " + diveWithAim(InputHandler.getStringStream("Day2/Dive.txt")));
+    }
+}
